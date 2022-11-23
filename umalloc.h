a106270225b3dc3c90823ed8876fbadfbb37cf3f
YAhtiW <102620528+YAhtiW@users.noreply.github.com>
2022-11-23 01:13:13 -0500

    Update umalloc.h

--- umalloc.h
+++ umalloc.h
@@ -1,11 +1,25 @@
+
+#ifndef UMALLOC_H_
+#define UMALLOC_H_
 #include <stdio.h>
 #include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
+#include <time.h>
+#include <sys/time.h>
+
 #define default_size 10485760
 
-#define malloc(x) umalloc(x, __FILE__, __LINE__)
-#define free(x) ufree(x, __FILE__, __LINE__)
+#define malloc(X) umalloc(X, __FILE__, __LINE__)
+#define free(Y) ufree(Y, __FILE__, __LINE__)
 
 void* umalloc( size_t, char*, int);
 void ufree( void*, char*, int);
+
+struct eblock *next(struct eblock *block);
+void initialize();
+void newEBlock(struct eblock *recentlyAllocated, int size, int index);
+void printArray();
+
+#endif
