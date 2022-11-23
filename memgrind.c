f44d096c74bbc315f0dfafaea032f02988af8aea
YAhtiW <102620528+YAhtiW@users.noreply.github.com>
2022-11-23 01:01:46 -0500

    Update memgrind.c

--- memgrind.c
+++ memgrind.c
@@ -1,19 +1,18 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+
 #include <unistd.h>
-#include <assert.h>
+
 #include <sys/types.h>
 #include <time.h>
 #include <sys/time.h>
 #include <errno.h>
 #include "umalloc.h"
 
-int main()
+int memorygrind()
 {
     int* x;
     int* y;
     int size;
+    int* pointers[10000];
 
     //case 0: Consistency
     x = (int *) malloc(1);
@@ -21,14 +20,15 @@ int main()
     int address0 = x;
     free(x);
     x = (int *) malloc(1);
-    printf("case 0:\naddress of the first pointer: %d\naddress of the second pointer: %d\n", address0, x);
+    int address1 = x;
+    printf("case 0:\ndo the addresses match? (1 is YES! 0 is NO!) %d\n", address0 == address1);
     free(x);
 
 
     //case 1: Maximization
     size = 1;
     x = (int *) malloc(size);
-    printf("case 1:\n");
+    printf("\ncase 1:\n");
     while(x != NULL)
     {
         printf("Success! size = %d, doubling...\n", size);
@@ -52,7 +52,7 @@ int main()
     //case 2: Basic Coalescence
     size = 1;
     x = (int *) malloc(size);
-    printf("case 2:\n");
+    printf("\ncase 2:\n");
     while(x != NULL)
     {
         free(x);
@@ -67,28 +67,14 @@ int main()
         x = (int *) malloc(size);
     }
     printf("allocated 1/2 of maximal allocation where size = %d\n", size);
-    size = 1;
-    y = (int *) malloc(size);
-    printf("case 2:\n");
-    while(y != NULL)
-    {
-        free(y);
-        size = size * 2;
-        y = (int *) malloc(size);
-    }
     size = size / 2;
     y = (int *) malloc(size);
-    while(y == NULL)
-    {
-        size = size / 2;
-        y = (int *) malloc(size);
-    }
     printf("allocated 1/4 of maximal allocation where size = %d\n", size);
     free(x);
     printf("freed pointer 1\n");
     free(y);
     printf("freed pointer 2\n");
-    size = default_size;
+    size = default_size - sizeof(struct eblock);
     x = (int *)malloc(size);
     if(x == NULL)
     {
@@ -98,5 +84,57 @@ int main()
     {
         printf("successfully allocated full memory!\n");
     }
+    free(x);
 
+
+    //case 3: saturation
+    printf("\ncase 3:\n");
+    for(int i = 0; i < 9216; i++)
+    {
+        pointers[i] = (int*)malloc(1024);
+    }
+    printf("pointers is now over 9000\n");
+    for(int i = 9216; i < 10000; i++)
+    {
+        pointers[i] = (int*)malloc(1);
+        size = i;
+        if(pointers[i] == NULL)
+        {
+            printf("\nSTOP\n");
+            break;
+        }
+    }
+    printf("saturation of space (without 9216 allocations): %d\n", (size-9217));
+
+    //case 4: time overhead
+    free(pointers[size]);
+    struct timeval c4Start, c4End;
+    printf("\ncase 4:\ntime start!\n");
+    gettimeofday(&c4Start, 0);
+    pointers[size] = (int*)malloc(1);
+    gettimeofday(&c4End, 0);
+    printf("time end!\n");
+    int totalTime = (c4End.tv_sec-c4Start.tv_sec) * 1000000 + (c4End.tv_usec-c4Start.tv_usec);
+    printf("max time overhead = %d ms\n", totalTime);
+    
+
+    //case 5: intermediate coalescence
+    printf("\ncase 5:\nfreeing memory...\n");
+    for(int i = 0; i < size+1; i++)
+    {
+        free(pointers[i]);
+    }
+    printf("freed all of pointers\n");
+    size = default_size-sizeof(struct eblock);
+    x = (int *)malloc(size);
+    if(x == NULL)
+    {
+        printf("failed to allocate full memory\n");
+    }
+    else
+    {
+        printf("successfully allocated full memory!\n");
+    }
+    free(x);
+    return 0;
 }
