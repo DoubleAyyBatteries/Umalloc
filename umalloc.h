e47f18f1ee7e6fc582734545809f258d6467bf84
YAhtiW <102620528+YAhtiW@users.noreply.github.com>
2022-11-22 23:16:11 -0500

    Update umalloc.h

--- umalloc.h
+++ umalloc.h
@@ -2,6 +2,10 @@
 #include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
+#define default_size 10485760
 
-void* umalloc( size_t bytes);
-void ufree( void* ptr);
+#define malloc(x) umalloc(x, __FILE__, __LINE__)
+#define free(x) ufree(x, __FILE__, __LINE__)
+
+void* umalloc( size_t, char*, int);
+void ufree( void*, char*, int);
